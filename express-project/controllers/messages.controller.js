2767d43e6ab13af774d12788d583cbf8c0ec02f0
Adi Akbar <adi.akbar91@gmail.com>
2022-03-24 06:11:23 +0700

    create layout in template engine

--- express-project/controllers/messages.controller.js
+++ express-project/controllers/messages.controller.js
@@ -1,8 +1,12 @@
 const path = require('path');
 
 function getMessages (req, res) {
-  res.sendFile(path.join(__dirname, '..', 'public', 'images', 'skimountain.jpg'))
+  // res.sendFile(path.join(__dirname, '..', 'public', 'images', 'skimountain.jpg'))
   // res.send('<ul><li>this is a list</li></ul>');
+  res.render('messages', {
+    title: 'Message to my friends',
+    friend: 'Adi Akbar'
+  });
 }
 
 function postMessage(req, res) {
